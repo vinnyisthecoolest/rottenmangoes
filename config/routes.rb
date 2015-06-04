7d02bd385203b19f62741e675475d166e9452236
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-06-04 17:54:55 -0400

    add log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
