97c2b6d7940be7e67b44f612813b13cd7b3fd4e9
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-06-04 14:22:07 -0400

    add user model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604010351) do
+ActiveRecord::Schema.define(version: 20150604180955) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150604010351) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
