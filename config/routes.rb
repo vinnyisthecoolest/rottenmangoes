cc0f0cc097f7f4c1989eeda0ed66130ed69c60c5
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-06-04 20:50:23 -0400

    add links to log in/out or sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
